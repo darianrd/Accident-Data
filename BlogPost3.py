63b5fc28cb41fbe5b9a5d239208c1cb5a647f24c
Darian <dyerdarian2@gmail.com>
2024-12-04 01:02:53 -0700

    Edit streamlit code

--- BlogPost3.py
+++ BlogPost3.py
@@ -80,8 +80,6 @@ with tab4:
     
     st.altair_chart(chart, use_container_width=True)
 
-
-
 color_scale = alt.Scale(domain = ['Daylight', 'Dark - Lighted', 'Dark - Not Lighted', 'Dawn', 'Dusk'],
                         range = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd'])
 
